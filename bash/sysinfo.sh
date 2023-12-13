7c065a379d7c4963a3459745e7d16f713f829946
Arshpreet Singh Rai <200517000@student.georgianc.on.ca>
2023-12-12 21:06:19 -0500

    Add sysinfo.sh script with improvements

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
-# Store inportant information in variables
-HOSTNAME=$hostname
-FQDN=$(hostname --fqdn)
-OS_NAME=$(lsb_release -d -s)
-IP_ADDRESS=$(ip route get 1 | awk '{print $NF;exit}')
-DISK_SPACE=$(df -h / | awk 'NR==2 {print $4}')
-#display the report using an output template
-cat <<EOF
-Report for $HOSTNAME
-====================
-FQDN: $FQDN
-Operating System name and version name: $OS_NAME
-IP Address: $IP_ADDRESS
-Root Filesystem Free Space: $DISK_SPACE
-====================
-EOF
+#Display information about a computer
+#output template
+output_template() {
+    echo ""
+    echo "Report for $hostname"
+    echo "=============="
+    echo "FQDN: $fqdn"
+    echo "Operating system name and version: $os_name $os_version"
+    echo "IP Address: $ip_address"
+    echo "Root Filesystem Free Space: $root_fs_free_space"
+    echo "=============="
+    echo ""
+}
+
+#Get hostnae and domain name
+hostname=$(hostname)
+fdqn=$(hostname --fdqn)
+
+#Get operating system information
+os_info=$(cat /etc/os-release | grep -E 'PRETTY_NAME|VERSION_ID')
+os_name=$(echo "$os_info" | grep -oP '(?<=PRETTY_NAME=")(.*?) (?=")')
+os_version=$(echo "$os_info" | grep -oP '(?<=VERSION_ID=")(.?=")')
+
+#Get IP Address
+ip_address=$(ip route | awk '/default/ {print $9}')
+
+#get root filesystem
+root_fs_free_space=$(df -h / | awk 'NR==2 {print $4}')
+
+#output the info
+output_template
+
